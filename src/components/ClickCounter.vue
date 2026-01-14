b3e47c836340d6b9b8179f01f9f7da415aa6f6ea
arnollder <arnoldermorozov@gmail.ru>
2026-01-14 16:05:47 +0300

    feat: вынес ClickCounter в отдельный компонент

---
+++ src/components/ClickCounter.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="counter">
+      <h2 :style="counterTitleColor">Counter</h2>
+      <div>Count: {{ count }}</div>
+      <div>Double count {{ doubleCount }}</div>
+    </div>
+    <button @click="increment">Increment++</button>
+    <button @click="decrement">Decrement--</button>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  computed: {
+    doubleCount() {
+      return this.count * 2
+    },
+    counterTitleColor() {
+      return { color: this.count > 5 ? 'red' : 'black' }
+    },
+  },
+  methods: {
+    showDecrement() {
+      console.log('decrement')
+    },
+    increment() {
+      this.count++
+    },
+
+    decrement() {
+      this.showDecrement()
+      this.count--
+    },
+    reset() {
+      this.count = 0
+    },
+  },
+  watch: {
+    count: {
+      handler(newValue, oldValue) {
+        console.log('newValue', newValue)
+        console.log('oldValue', oldValue)
+        // if (newValue === 0) alert('Значение равно 0!')
+      },
+      immediate: true,
+      deep: true,
+    },
+  },
+})
+</script>
+
+<style scoped>
+button {
+  padding: 5px 10px;
+  border: none;
+  background-color: rgb(74, 226, 74);
+  border-radius: 5px;
+  margin-right: 5px;
+  cursor: pointer;
+  transition: 0.5s;
+}
+button:hover {
+  color: darkslategray;
+}
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="root">
+    <header class="header">Header</header>
+    <section class="content">
+      <h1>Main Page</h1>
+      <ClickCounter />
+    </section>
+    <footer class="footer">Footer {{ new Date().getFullYear() }}</footer>
+  </div>
+</template>
+
+<script lang="ts">
+import ClickCounter from '@/components/ClickCounter.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TestComponent',
+  components: { ClickCounter },
+})
+</script>
+
+<style scoped>
+.root {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.header,
+.footer {
+  height: 100px;
+  background-color: gray;
+  font-size: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.content {
+  flex-grow: 1;
+  padding: 10px;
+}
+</style>
